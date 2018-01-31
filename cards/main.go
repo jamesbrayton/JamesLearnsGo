46cc7bb4b2ba91fa1ebccb57522dfc7cda624bc0
james brayton <jamesgbrayton@gmail.com>
2018-01-31 11:53:27 -0700

    Adding variable creation example

--- cards/main.go
+++ cards/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	// This is long hand for the same variable creation and assignment used below
+	// var card string = "Ace of Spades"
+
+	// := is only needed during creation of a new variable.  = is used for re-assignment or update of value
+	card := "Ace of Spades"
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
 }
