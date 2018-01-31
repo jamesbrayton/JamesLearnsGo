1d8cdc0a2d5cbefc78c9f57de3bc95d7262250d3
james brayton <jamesgbrayton@gmail.com>
2018-01-31 12:00:07 -0700

    Updated to use function to return card value.

--- cards/main.go
+++ cards/main.go
@@ -5,12 +5,11 @@ import (
 )
 
 func main() {
-	// This is long hand for the same variable creation and assignment used below
-	// var card string = "Ace of Spades"
-
-	// := is only needed during creation of a new variable.  = is used for re-assignment or update of value
-	card := "Ace of Spades"
-	card = "Five of Diamonds"
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
