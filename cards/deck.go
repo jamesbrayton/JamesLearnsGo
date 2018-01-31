80320bd1dba425b49ee8698f918f1f9851ebb88e
james brayton <jamesgbrayton@gmail.com>
2018-01-31 12:40:17 -0700

    Updating naming to clarify what is going on code

--- cards/deck.go
+++ cards/deck.go
@@ -8,8 +8,11 @@ import (
 // which is a slice of strings
 type deck []string
 
-func (deckInstance deck) print() {
-	for i, card := range deckInstance {
+// convention in a reciever is to refer to the instance with a one or two variable as in
+// func (d deck) print()
+// modified as thisDeck to relate to the idea of 'this' being use in Go
+func (thisDeck deck) print() {
+	for i, card := range thisDeck {
 		fmt.Println(i, card)
 	}
 }
