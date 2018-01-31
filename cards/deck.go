cf740425336880741c35850088a2d7fdb6380d98
james brayton <jamesgbrayton@gmail.com>
2018-01-31 13:10:18 -0700

    Updating deck type to build itself a deck of cards when requesting a new deck with newDeck() function

--- cards/deck.go
+++ cards/deck.go
@@ -8,6 +8,23 @@ import (
 // which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	// variable _ is used when we are saying we understand a variable exists here (in this case our iterator), but we aren't actually
+	// going to use it for anything.  This keeps the compiler from complaining.
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+
+	return cards
+}
+
 // convention in a reciever is to refer to the instance with a one or two variable as in
 // func (d deck) print()
 // modified as thisDeck to relate to the idea of 'this' being use in Go
