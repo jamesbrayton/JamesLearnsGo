d135741cb438acc1404e86f05f9e02c2b21a31b7
James <James.brayton@nordstrom.com>
2018-02-01 11:15:45 -0700

    Added test for newdeck.

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,1 +1,12 @@
 package main
+
+import "testing"
+
+func TestNewdeck(t *testing.T) {
+	d := newDeck()
+
+	// Verify that we have 52 cards in our deck.
+	if len(d) != 52 {
+		t.Errorf("Expected deck length of 52, but got %v", len(d))
+	}
+}
