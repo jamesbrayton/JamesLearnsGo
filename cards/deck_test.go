d7fe5d14a14266edcac966987f2b5bcec6808fa1
James <James.brayton@nordstrom.com>
2018-02-01 11:25:43 -0700

    Splitting tests out

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -2,17 +2,34 @@ package main
 
 import "testing"
 
-func TestNewdeck(t *testing.T) {
+// Verify that we have 52 cards in our deck.
+func TestNewDeckHasCorrectNumberOfCards(t *testing.T) {
 	d := newDeck()
 
-	// Verify that we have 52 cards in our deck.
 	if len(d) != 52 {
 		t.Errorf("Expected deck length of 52, but got %v", len(d))
 	}
+}
 
-	// Verify some ordering of the deck by testing the first and last cards.
-	if d[0] != "Ace of Spades" {
-		t.Errorf("Expected card "Ace of Spades", but got %v", d[0])
+// Verify some strict ordering of the deck by testing the first card.
+func TestFirstCardInNewDeckIsAceOfSpades(t *testing.T) {
+	d := newDeck()
+
+	testCard := "Ace of Spades"
+
+	if d[0] != testCard {
+		t.Errorf("Expected card %v, but got %v", testCard, d[0])
+	}
+}
+
+// Verify some strict ordering of the deck by testing the last card.
+func TestLastCardInNewDeckIsKingOfClubs(t *testing.T) {
+	d := newDeck()
+
+	testCard := "King of Clubs"
+
+	if d[len(d)-1] != testCard {
+		t.Errorf("Expected card '%v', but got %v", testCard, d[len(d)-1])
 	}
 }
 
