8a686f7eaa3e158e78522ac43f23cbe60f99d034
James <James.brayton@nordstrom.com>
2018-02-01 11:17:44 -0700

    Added test for newdeck.

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -9,4 +9,13 @@ func TestNewdeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected deck length of 52, but got %v", len(d))
 	}
+
+	// Verify some ordering of the deck by testing the first and last cards.
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected card "Ace of Spades", but got %v", d[0])
+	}
+}
+
+func TestShuffleDeck(t *testing.T) {
+
 }
