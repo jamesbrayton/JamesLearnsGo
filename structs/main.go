a07c51bd7ae39185e043748b84dfc7808370dd8d
James <James.brayton@nordstrom.com>
2018-02-02 15:10:16 -0700

    Added third form of creating and assigning values to structs

--- structs/main.go
+++ structs/main.go
@@ -10,7 +10,15 @@ type person struct {
 }
 
 func main() {
-	alex := person{"Alex", "Anderson"}
+	// alex := person{"Alex", "Anderson"}
+	// alex := person{firstName: "Alex", lastName: "Anderson"}
+	var alex person
+
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
 
 	fmt.Println(alex)
+
+	// Prints out all the field names plus their values.
+	fmt.Printf("%+v", alex)
 }
