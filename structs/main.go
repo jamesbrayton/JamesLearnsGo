b742587fab79abb4d305868061d188da564efb12
James <James.brayton@nordstrom.com>
2018-02-02 15:16:35 -0700

    Added contactInfo struct which was added to person.

--- structs/main.go
+++ structs/main.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 )
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
@@ -16,9 +22,22 @@ func main() {
 
 	alex.firstName = "Alex"
 	alex.lastName = "Anderson"
+	alex.contact.email = "alex37@lougle.com"
+	alex.contact.zipCode = 12345
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "partyanimal@beer.com",
+			zipCode: 94000,
+		},
+	}
 
 	fmt.Println(alex)
 
 	// Prints out all the field names plus their values.
 	fmt.Printf("%+v", alex)
+
+	fmt.Printf("%+v", jim)
 }
