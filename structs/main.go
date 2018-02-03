eb94054252cf76768380ab5c1dcf562f5debcdfb
james brayton <jamesgbrayton@gmail.com>
2018-02-03 11:06:32 -0700

    Updated to use pointers and added notes

--- structs/main.go
+++ structs/main.go
@@ -39,17 +39,29 @@ func main() {
 	// Prints out all the field names plus their values.
 	fmt.Printf("%+v\n", alex)
 
+	// Use our new print function.
 	jim.print()
 
-	jim.updateName("Jimbo")
+	// Create a pointer to the jim object.
+	jimPointer := &jim
+
+	// NOTES:
+	// varaiable - Give me the value this variable has at its memory address. (value)
+	// &variable - Give me the memory address of the value this variable is pointing at (reference).
+	// *pointer - Give me the value this memory address is pointing at (pointer).
+
+	// Update jim using the pointer.
+	jimPointer.updateFirstName("Jimbo")
 
 	jim.print()
 }
 
-func (p person) updateName(newFirstName string) {
-	p.firstName = newFirstName
+// Update name on the person using pointers.
+func (pointerToPerson *person) updateFirstName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
 }
 
+// Print the person object formatted to include all possible value names.
 func (p person) print() {
 	fmt.Printf("%+v\n", p)
 }
