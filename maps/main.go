c090d1d671373dfb90b5a08e2fd144d29807ac2c
James <James.brayton@nordstrom.com>
2018-02-05 12:16:46 -0700

    Added map example code. Removing testing file for now.

--- maps/main.go
+++ maps/main.go
@@ -1,9 +1,40 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	// Literal syntax for creating a map.
-	colors := map[string]string{
-		"red": "#ff0000"
+	colors1 := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
 	}
 
+	// Print out the contents of the colors1 map.
+	fmt.Println(colors1)
+
+	// Alternate syntax
+	var colors2 map[string]string
+
+	// Print out the contents of the colors2 map.
+	fmt.Println(colors2)
+
+	// Third syntax using make()
+	colors3 := make(map[string]string)
+
+	// Print out the contents of the colors3 map.
+	fmt.Println(colors3)
+
+	// Add "white" to the colors3 map.
+	colors3["white"] = "#ffffff"
+
+	// print out the colors3 map to see that white was added.
+	fmt.Println(colors3)
+
+	// Delete function example
+	delete(colors3, "white")
+
+	// print out the colors3 map to see that white was deleted.
+	fmt.Println(colors3)
 }
