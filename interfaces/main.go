c5da460ed3b403aa734a5278238bd965d7cffbc1
James <James.brayton@nordstrom.com>
2018-02-05 13:15:04 -0700

    Added interface for bots

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,17 +1,39 @@
 package main
 
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
+// English bot
 type englishBot struct{}
 
+// Spanish bot
 type spanishBot struct{}
 
 func main() {
+	// Create english and spanish bots.
+	eb := englishBot{}
+	sb := spanishBot{}
 
+	// Print the greetings for the 2 bots.
+	printGreeting(eb)
+	printGreeting(sb)
 }
 
-func (eb englishBot) getGreeting() string {
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+// Greeting function for the english bot.
+// NOTE: we are only defining the type on the
+// receiver since we are not using a variable.
+func (englishBot) getGreeting() string {
 	return "Hello!"
 }
 
-func (sb spanishBot) getGreeting() string {
+// Greeting function for the spanish bot.
+func (spanishBot) getGreeting() string {
 	return "Hola!"
 }
