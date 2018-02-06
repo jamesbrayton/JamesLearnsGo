e344d03e477f78d2ae71c7021119beee76d00d94
James <James.brayton@nordstrom.com>
2018-02-06 11:43:56 -0700

    Added failure scenario.

--- channels/main_test.go
+++ channels/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 	"testing"
 
@@ -11,13 +12,19 @@ import (
 type MockHttpClient struct {
 }
 
+// Function for overriding Get behavior on an http.client in order to test.
 func (m MockHttpClient) Get(url string) (resp *http.Response, err error) {
+	// Create failure scenario to test both positive and negative behavior.
+	if url == "https://should.fail" {
+		return new(http.Response), errors.New("no mi gusta")
+	}
+
 	return new(http.Response), nil
 }
 
 // Tests the check link function.  The function returns a boolean value
 // indicating the success of the call and a string message explaining the result.
-func TestCheckLink(t *testing.T) {
+func TestCheckLinkSucceeds(t *testing.T) {
 	// Mock http client.
 	mockClient := MockHttpClient{}
 
@@ -32,6 +39,25 @@ func TestCheckLink(t *testing.T) {
 	assert.Contains(t, message, testLink)
 }
 
+// Tests the check link function under a failure scenario.  The function returns a
+// boolean value indicating the success of the call and a string message explaining
+// the result.
+func TestCheckLinkFail(t *testing.T) {
+	// Mock http client.
+	mockClient := MockHttpClient{}
+
+	// Link to test with the mock client.
+	testLink := "https://should.fail"
+
+	// Call the code we are testing.
+	success, message := checkLink(mockClient, testLink)
+
+	// Assert the expectations we met.
+	assert.False(t, success)
+	assert.Contains(t, message, testLink)
+}
+
+// Test the getLinks() function.
 func TestGetLinks(t *testing.T) {
 	expectedLinks := []string{
 		"https://google.com",
