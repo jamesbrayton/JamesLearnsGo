61e2644c24d1e283f2cd99b1999ababdc17ca803
James <James.brayton@nordstrom.com>
2018-02-06 11:35:48 -0700

    Changes to include interface for httpClient in order to make testing easier.

--- channels/main.go
+++ channels/main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+// Interface for defining an http client.  This is used for testing later.
+type httpClient interface {
+	Get(url string) (*http.Response, error)
+}
+
+// Main function.
+func main() {
+	links := getLinks()
+
+	for _, link := range links {
+		fmt.Println(checkLink(new(http.Client), link))
+	}
+}
+
+// Check link function that returns a boolean value indicating the success of the call
+// and a string message explaining the result.  This takes an httpClient interface and
+// a string for the link to call.
+func checkLink(hc httpClient, link string) (bool, string) {
+	// Make a get request against the link.
+	_, err := hc.Get(link)
+
+	// If we encountered an error, return false and an error message.
+	if err != nil {
+		return false, link + " might be down."
+	}
+
+	// If no error, return true and a message.
+	return true, link + " is up."
+}
+
+// Function for returning links.
+func getLinks() []string {
+	return []string{
+		"https://google.com",
+		"https://facebook.com",
+		"https://stackoverflow.com",
+		"https://golang.org",
+		"https://amazon.com",
+	}
 }
--- channels/main_test.go
+++ channels/main_test.go
@@ -1,14 +1,47 @@
 package main
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMain(t *testing.T) {
-	example1 := 1
-	example2 := 2
+// A mock http object for testing.
+type MockHttpClient struct {
+}
 
-	assert.NotEqual(t, example1, example2)
+func (m MockHttpClient) Get(url string) (resp *http.Response, err error) {
+	return new(http.Response), nil
+}
+
+// Tests the check link function.  The function returns a boolean value
+// indicating the success of the call and a string message explaining the result.
+func TestCheckLink(t *testing.T) {
+	// Mock http client.
+	mockClient := MockHttpClient{}
+
+	// Link to test with the mock client.
+	testLink := "https://yourMom.com"
+
+	// Call the code we are testing.
+	success, message := checkLink(mockClient, testLink)
+
+	// Assert the expectations we met.
+	assert.True(t, success)
+	assert.Contains(t, message, testLink)
+}
+
+func TestGetLinks(t *testing.T) {
+	expectedLinks := []string{
+		"https://google.com",
+		"https://facebook.com",
+		"https://stackoverflow.com",
+		"https://golang.org",
+		"https://amazon.com",
+	}
+
+	actualLinks := getLinks()
+
+	assert.Equal(t, expectedLinks, actualLinks)
 }
