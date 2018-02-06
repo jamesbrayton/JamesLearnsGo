f18a44cbbb92d7e15a30b7bd98f6c57b9ad333d7
James <James.brayton@nordstrom.com>
2018-02-06 11:52:02 -0700

    Removed unused variable in function receiver"

--- channels/main.go
+++ channels/main.go
@@ -10,29 +10,34 @@ type httpClient interface {
 	Get(url string) (*http.Response, error)
 }
 
+type checkResponse struct {
+	success bool
+	message string
+}
+
 // Main function.
 func main() {
 	links := getLinks()
 
 	for _, link := range links {
-		fmt.Println(checkLink(new(http.Client), link))
+		fmt.Printf("%v", checkLink(new(http.Client), link))
 	}
 }
 
 // Check link function that returns a boolean value indicating the success of the call
 // and a string message explaining the result.  This takes an httpClient interface and
 // a string for the link to call.
-func checkLink(hc httpClient, link string) (bool, string) {
+func checkLink(hc httpClient, link string) checkResponse {
 	// Make a get request against the link.
 	_, err := hc.Get(link)
 
 	// If we encountered an error, return false and an error message.
 	if err != nil {
-		return false, link + " might be down."
+		return checkResponse{success: false, message: link + " might be down."}
 	}
 
 	// If no error, return true and a message.
-	return true, link + " is up."
+	return checkResponse{success: true, message: link + " is up."}
 }
 
 // Function for returning links.
